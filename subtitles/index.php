c2543249472b9215c894229c5300647c4f670a16
mblonyox <mblonyox@yahoo.co.id>
2016-02-29 17:01:28 +0700

    Upload all files.
    First commit. Just mass upload.

---
+++ subtitles/index.php
@@ -0,0 +1,69 @@
+<?php
+	
+?>
+<!DOCTYPE html>
+<html lang=en>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Subtitle by mblonyox</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+	<div class="site-wrapper">
+		<div class="site-wrapper-inner">
+			<div class="cover-container">
+				<div class="masthead clearfix">
+					<div class="inner">
+						<h3 class="masthead-brand">Subtitles</h3>
+						<nav>
+							<ul class="nav masthead-nav">
+								<li>
+									<a href="#">Home</a>
+								</li>
+								<li>
+									<a href="#">Popular</a>
+								</li>
+								<li>
+									<a href="#">Recent</a>
+								</li>
+								<li>
+									<a href="#">F.A.Q</a>
+								</li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+				<div class="inner cover">
+					<form class="form-horizontal">
+						<div class="form-group">
+							<div class="col-sm-10">
+								<input type="text" name="s" placeholder="Search..." class="form-control input-lg search-query">
+							</div>
+							<button type="submit" class="btn btn-lg btn-default">Go</button>
+						</div>
+						<div class="form-group">
+							<div class="col-sm-4">
+								<label class="radio-inline">
+									<input type="radio" name="l" id="langEng" value="en" checked>English
+								</label>
+							</div>
+							<div class="col-sm-4">
+								<label class="radio-inline">
+									<input type="radio" name="l" id="langId" value="id">Bahasa Indonesia
+								</label>
+							</div>
+						</div>
+					</form>
+				</div>
+				<div class="mastfoot">
+				</div>
+			</div>
+		</div>
+	</div>
+	<script src="js/jquery-2.2.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
