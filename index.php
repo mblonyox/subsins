8459ceea275b00e68b238b6ecd3bf663ff354101
mblonyox <mblonyox@yahoo.co.id>
2016-03-04 16:38:04 +0700

    progress-weekend
    Progress for weekend.

--- index.php
+++ index.php
@@ -2,13 +2,77 @@
 	
 	require 'lib/simple_html_dom.php';
 
+	if (isset($_GET['d'])) {
+		$html = file_get_html('http://v2.subscene.com'.$_GET['d']);
+		$dllink = $html->find('a#downloadButton',0)->href;
+		$data = file_get_contents('http://subscene.com'.$dllink);
+		$file_name = trim(str_replace( array('/', '.aspx'), '_', $_GET['d']), '_').'.zip';
+		header("Content-Type: application/zip");
+	    header("Content-Disposition: attachment; filename=$file_name");
+	    header("Content-Length: " . strlen($data));
+	    echo $data;
+	    exit;
+	}
+
+	if (isset($_GET['title'])) {
+		$title = $_GET['title'];
+		$html = file_get_html('http://subscene.com/subtitles/' . $title);
+		$byFilm = $html->find('div.byFilm', 0);
+		if (!is_null($byFilm)) {
+			$film = array();
+			if (!empty($byFilm->find('div.poster img', 0)->src)) {
+				$film['poster'] = $byFilm->find('div.poster img',0)->src;
+			}
+			$film['title'] = $byFilm->find('div.header h2', 0)->plaintext;
+			$imdburl = $byFilm->find('div.header', 0)->find('a.imdb', 0)->href;
+			if (!filter_var($imdburl, FILTER_VALIDATE_URL) === false) {
+				$film['imdb'] = $imdburl;
+			}
+			$film['year'] = $byFilm->find('div.header ul li', 0)->innertext;
+		}
+		$content = $byFilm->find('div.content', 0);
+		if(!is_null($content)){
+
+		}
+	}
+
+	if (isset($_GET['s'])) {
+		$searchquery = $_GET['s'];
+
+		try {
+			$html = file_get_html('http://subscene.com/subtitles/title?q=' . urlencode($searchquery));
+			$content = $html->find('div.search-result', 0);
+			if (!is_null($content)) {
+				foreach ($content->find('h2') as $h2) {
+					$h2->class = 'group';
+					foreach ($content->find('ul') as $ul) {
+						$ul->class = 'list-group';
+						foreach ($ul->find('li') as $li) {
+							$li->class = 'list-group-item';
+							$a = $li->find('div.title a', 0);
+							$a->class = 'list-group-item-heading';
+							$a->href = str_replace('/subtitles/', '?title=' , $a->href );
+						}
+					}
+				}
+				$content->find('div.alternativeSearch', 0)->outertext = '';
+			}
+			
+		} catch (Exception $e) {
+			
+		}
+	}
+/*
 	if (isset($_GET['q'])) {
 		$query = $_GET['q'];
 		$language = array("English", "Indonesian", "Malay");
+		$cookie = "subscene_sLanguageIds=44-13-50; subscene_sLanguageNames=%28Indonesian%2C%20English%2C%20Malay%29";
+		$opts = array('http' => array('header'=> 'Cookie: ' . $cookie .'\r\n'));
+		$context = stream_context_create($opts);
 
 		try {
 
-			$html = file_get_html('http://v2.subscene.com/s.aspx?q=' . urlencode($query));
+			$html = file_get_html('http://v2.subscene.com/s.aspx?q=' . urlencode($query) , false, $context);
 
 			$content = $html->find('table.filmSubtitleList', 0);
 			if (!is_null($content)) {
@@ -55,6 +119,7 @@
 #		print_r($subs);
 #		exit;
 	}
+*/
 ?>
 <!DOCTYPE html>
 <html lang=en>
@@ -63,6 +128,8 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Subtitle by mblonyox</title>
+	<link rel="shortcut icon" type="image/x-icon" href="/template/favicon.ico">
+	<link rel="icon" type="image/x-icon" href="/template/favicon.ico">
 	<link href="css/bootstrap.min.css" rel="stylesheet">
 	<link href="css/style.css" rel="stylesheet">
 </head>
@@ -95,7 +162,7 @@
 					<form class="form-horizontal">
 						<div class="form-group">
 							<div class="col-sm-10">
-								<input type="text" name="q" placeholder="Search..." class="form-control input-lg search-query" <?php if(isset($query)) {echo 'value="'.$query.'"';} ?>>
+								<input type="text" name="s" placeholder="Search..." class="form-control input-lg search-query" <?php if(isset($searchquery)) {echo 'value="'.$searchquery.'"';} ?>>
 							</div>
 							<button type="submit" class="btn btn-lg btn-default">Go</button>
 						</div>
@@ -113,19 +180,29 @@
 						</div>
 					</form>
 					<?php
-					if(isset($subs) && !is_null($subs)) {
-						echo '<hr/><table class="table table-hover"><tr><th>#</th><th>Language</th><th>Release name</th><th>Rate</th><th>Author</th></tr>';
-						$i = 1;
-						foreach ($subs as $sub) {
-							$l = $sub['language'];
-							$t = $sub['title'];
-							$u = $sub['url'];
-							$r = $sub['rating'];
-							$ul = $sub['uploader'];
-							echo "<tr><td>$i</td><td>$l</td><td><a href='?d=$u'>$t</a></td><td>$r</td><td>$ul</td></tr>";
-							$i++;
+/*					if(isset($subs)) {
+						if (empty($subs)) {
+							echo '<hr/><h2>No subtitles found.</h2>';
+						}else{							
+							echo '<hr/><table class="table table-hover"><tr><th>#</th><th>Language</th><th>Release name</th><th>Rate</th><th>Author</th></tr>';
+							$i = 1;
+							foreach ($subs as $sub) {
+								$l = $sub['language'];
+								$t = $sub['title'];
+								$u = $sub['url'];
+								$r = $sub['rating'];
+								$ul = $sub['uploader'];
+								echo "<tr><td>$i</td><td>$l</td><td><a href='?d=$u'>$t</a></td><td>$r</td><td>$ul</td></tr>";
+								$i++;
+							}
+							echo '</table>';
 						}
-						echo '</table>';
+					}
+*/
+
+					if(isset($content)) {
+						echo "<hr/>";
+						echo $content;
 					}
 					?>
 				</div>
